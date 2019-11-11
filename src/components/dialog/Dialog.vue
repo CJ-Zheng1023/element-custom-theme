1e62584e6fd46a0fc0125ba8bb061cfce92bd064
fandanping <18602474229@163.com>
2019-11-11 10:40:51 +0800

    dialog change

--- src/components/dialog/Dialog.vue
+++ src/components/dialog/Dialog.vue
@@ -5,10 +5,12 @@
       type="success" class="desc">
     </el-alert>
     <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
+    <el-button type="text" @click="dialogTableVisible = true">点击打开嵌套表格的Dialog</el-button>
     <el-dialog
       title="提示"
       :visible.sync="dialogVisible"
       width="30%"
+      class="el-dialog-table"
       >
       <span>这是一段信息</span>
       <span slot="footer" class="dialog-footer">
@@ -16,6 +18,45 @@
         <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
       </span>
     </el-dialog>
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogTableVisible"
+      width="70%"
+    >
+      <el-table
+        :data="tableData"
+        border
+        stripe
+        style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址">
+        </el-table-column>
+        <el-table-column
+          label="操作">
+          <template slot-scope="scope">
+            <el-button type="success" icon="el-icon-view" size="mini" round plain>查 看</el-button>
+            <el-button type="primary" icon="el-icon-edit" size="mini" round plain>编 辑</el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini" round plain>删 除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+
     <el-card class="box-card use-desc">
       <div slot="header" class="clearfix">
         <span>使用说明</span>
@@ -34,11 +75,62 @@
   </div>
 </template>
 <script>
+import ElDialog from '../../../node_modules/element-ui/packages/dialog/src/component.vue'
+
 export default {
+  components: {ElDialog},
   name: 'Dialog',
   data () {
     return {
-      dialogVisible: false
+      dialogVisible: false,
+      dialogTableVisible: false,
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄',
+        isRead: true
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄',
+        isRead: false
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄',
+        isRead: false
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄',
+        isRead: false
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄',
+        isRead: false
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄',
+        isRead: false
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄',
+        isRead: false
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄',
+        isRead: false
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄',
+        isRead: false
+      }]
+
     }
   },
   methods: {
