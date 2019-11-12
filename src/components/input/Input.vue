da63c5a0ba565fd30e6391d3796411a0e923340a
fandanping <18602474229@163.com>
2019-11-12 10:20:41 +0800

    input change

--- src/components/input/Input.vue
+++ src/components/input/Input.vue
@@ -24,13 +24,60 @@
         <el-input v-model="form.name"></el-input>
       </el-form-item>
     </el-form>
+    <div class="demo-input-suffix">
+      <p>带icon的输入框-属性方式：</p>
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-input
+            placeholder="请选择日期"
+            suffix-icon="el-icon-date"
+            v-model="input1">
+          </el-input>
+        </el-col>
+        <el-col :span="12">
+          <el-input
+            placeholder="请输入内容"
+            prefix-icon="el-icon-search"
+            v-model="input2">
+          </el-input>
+        </el-col>
+      </el-row>
+    </div>
+    <div class="demo-input-suffix">
+      <p>带icon的输入框-slot 方式：</p>
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <el-input
+            placeholder="请选择日期"
+            v-model="input3">
+            <i slot="suffix" class="el-input__icon el-icon-date"></i>
+          </el-input>
+        </el-col>
+        <el-col :span="12">
+          <el-input
+            placeholder="请输入内容"
+            v-model="input4">
+            <i slot="prefix" class="el-input__icon el-icon-search"></i>
+          </el-input>
+        </el-col>
+      </el-row>
+    </div>
+    <div>
+      <p>文本域：</p>
+      <el-input
+        type="textarea"
+        :rows="2"
+        placeholder="请输入内容"
+        >
+      </el-input>
+    </div>
     <el-card class="box-card use-desc">
       <div slot="header" class="clearfix">
         <span>使用说明</span>
       </div>
       <div>
         <p><span>@inputHeight: 40px;</span><span class="item">---输入框-高度</span></p>
-        <p><span>@inputBorderRadius: 0px; </span><span class="item">---输入框-圆角尺寸</span></p>
+        <p><span>@inputBorderRadius: 4px; </span><span class="item">---输入框-圆角尺寸</span></p>
         <p><span>@inputMediumFontSize: 14px; </span><span class="item">--- 中等输入框-字体大小</span></p>
         <p><span>@inputMediumHeight: 36px;</span><span class="item">---中等输入框-高度</span></p>
         <p><span>@inputMediumLineHeight: 36px;</span><span class="item">---中等输入框-行高</span></p>
@@ -40,6 +87,8 @@
         <p><span>@inputMiniFontSize: 12px;</span><span class="item">---迷你输入框</span></p>
         <p><span>@inputMiniHeight: 28px;</span><span class="item">---迷你输入框-高度</span></p>
         <p><span>@inputMiniLineHeight: 28px; </span><span class="item">--迷你输入框-行高</span></p>
+        <p><span>@inputHoverBorderColor: #C0C4CC;</span><span class="item">--输入框- 悬浮-边框颜色</span></p>
+        <p><span>@TextareaBorderRaius: 4px;</span><span class="item">--文本域- 边框圆角</span></p>
       </div>
     </el-card>
   </div>
@@ -51,7 +100,11 @@ export default {
     return {
       form: {
         name: ''
-      }
+      },
+      input1: '',
+      input2: '',
+      input3: '',
+      input4: ''
     }
   }
 }
